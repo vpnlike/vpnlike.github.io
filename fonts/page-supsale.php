c365d7f2979ac4f2c04fb728f55200ef55277f6a
vpnlike <65229579+vpnlike@users.noreply.github.com>
2025-03-15 17:23:35 +0330

    Add files via upload

---
+++ fonts/page-supsale.php
@@ -0,0 +1,25 @@
+<?php 
+	/* Template Name: برگه تماس با ما */
+	get_header();
+?>
+<!-- telegram -->
+		<!-- slide -->
+		<div class="slide">
+		<img src="<?php bloginfo('template_directory'); ?>/image/contact.jpg">
+		</div>
+		<!-- /slide -->
+		
+		<!-- contact -->
+		<div class="contacttitr">
+			<h2>لطفا برای ارتباط با کارشناسان سایت ما به <span> لینک تلگرام </span> زیر مراجعه کنید<h2>
+				<hr>
+			<a href="http://telegram.me/vpnlike1" title="پشتیبانی تلگرام"><img src="<?php bloginfo('template_directory'); ?>/image/contact/support-telegram-contact.png"></a>
+			<a href="http://telegram.me/vpnlike" title="کانال تلگرام"><img src="<?php bloginfo('template_directory'); ?>/image/contact/channel-telegram-contact.png"></a>
+			<a href="" title="آدرس ایمیل ما"><img src="<?php bloginfo('template_directory'); ?>/image/contact/support-email-contact.png"></a>
+		</div>
+		<!-- /contact -->
+		
+		
+	</div>
+	
+<?php get_footer(); ?>
